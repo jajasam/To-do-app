1255ea94335f73f5a16127396308b0645718fb24
Jana <jana.samson@hotmail.fr>
2022-07-05 11:34:55 -0400

    use of a todo list class

--- script.js
+++ script.js
@@ -1,10 +1,59 @@
 const themeIcons = document.querySelectorAll(".theme-icon");
-const body = document.querySelector("body")
+const body = document.querySelector("body");
+const input = document.querySelector("input");
+const listContainer = document.querySelector('.todo-list');
 
-function toggleTheme(e) {
-    body.classList.toggle("light");
-    body.classList.toggle("dark")
-}
+class TodoList {
+    list = [];
 
+    toggleTheme = function(e) {
+        body.classList.toggle("light");
+        body.classList.toggle("dark");
+        themeIcons.forEach(icon => icon.classList.toggle("hidden"));
+    }
 
-themeIcons.forEach(icon => icon.addEventListener("click", toggleTheme));
+    addItem = function(thingToDo, isCompleted) {
+        const newItem = {thingToDo, isCompleted}
+        this.list.push(newItem);
+        this.render();
+    }
+
+    removeItem = function(itemIndex) {
+        this.list.splice(itemIndex);
+        this.render();
+    }
+
+    toggleIsCompleted = (i) => {
+        this.list[i].isCompleted = !this.list[i].isCompleted;
+        this.render();
+    }
+
+    reset = function() {
+        this.list = [];
+        this.render();
+    }
+
+    render = function(filter) {
+        let html = "";
+        this.list.forEach(({thingToDo, isCompleted}, i) => 
+            html += 
+            `<div class="todo-elem" id="${i}" onClick="todoList.toggleIsCompleted(${i})" draggable="true" ondragover="">
+                <span class="icon ${isCompleted ? "checked-icon" : "unchecked-icon"}"></span>
+                <p>${thingToDo}</p>
+                <span class="clear-icon" onClick="removeItem(${i})"></span>
+            </div>`
+        )
+        listContainer.innerHTML = html;
+    }
+};
+
+const todoList = new TodoList();
+
+themeIcons.forEach(icon => icon.addEventListener("click", todoList.toggleTheme));
+
+input.addEventListener("keyup", function(e) {
+    e.preventDefault();
+    if ((e.key === 'Enter')) {
+        todoList.addItem(e.target.value, false);
+    }
+});
