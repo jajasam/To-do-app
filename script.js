262b560431b6fb468e1562c57ffc36f38cbe14a4
Jana <jana.samson@hotmail.fr>
2022-07-05 14:30:31 -0400

    draggable to-do elem

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const themeIcons = document.querySelectorAll(".theme-icon");
 const body = document.querySelector("body");
 const input = document.querySelector("input");
@@ -5,6 +6,7 @@ const listContainer = document.querySelector('.todo-list');
 
 class TodoList {
     list = [];
+    draggedItemIndex;
 
     toggleTheme = function(e) {
         body.classList.toggle("light");
@@ -23,8 +25,31 @@ class TodoList {
         this.render();
     }
 
-    toggleIsCompleted = (i) => {
-        this.list[i].isCompleted = !this.list[i].isCompleted;
+    toggleIsCompleted = function(id) {
+        this.list[id].isCompleted = !this.list[id].isCompleted;
+        this.render();
+    }
+
+    dragStart = function(id) {
+        this.draggedItemIndex = id;
+    }
+
+    dragOver = function(id) {
+        const elems = document.querySelectorAll(".todo-elem");
+        elems.forEach(elem => elem.classList.remove("dragged-on"));
+        document.getElementById(id).classList.add("dragged-on");
+    }
+
+    drop = function() {
+        const elems = document.querySelectorAll(".todo-elem");
+        let index;
+        elems.forEach(elem => {
+            elem.classList.contains("dragged-on") ? index = elem.id : elem
+        });
+        const item = this.list[this.draggedItemIndex];
+        this.list.splice(this.draggedItemIndex, 1);
+        this.list.splice(index, 0, item);
+
         this.render();
     }
 
@@ -37,10 +62,19 @@ class TodoList {
         let html = "";
         this.list.forEach(({thingToDo, isCompleted}, i) => 
             html += 
-            `<div class="todo-elem" id="${i}" onClick="todoList.toggleIsCompleted(${i})" draggable="true" ondragover="">
-                <span class="icon ${isCompleted ? "checked-icon" : "unchecked-icon"}"></span>
-                <p>${thingToDo}</p>
-                <span class="clear-icon" onClick="removeItem(${i})"></span>
+            `<div 
+                class="todo-elem"
+                id="${i}"
+                draggable="true"
+
+                onClick="todoList.toggleIsCompleted(${i})"
+                ondragover="todoList.dragOver(${i})"
+                ondragstart="todoList.dragStart(${i})"
+                ondrop="todoList.drop()"
+                >
+                    <span class="icon ${isCompleted ? "checked-icon" : "unchecked-icon"}"></span>
+                    <p>${thingToDo}</p>
+                    <span class="clear-icon" onClick="removeItem(${i})"></span>
             </div>`
         )
         listContainer.innerHTML = html;
@@ -55,5 +89,10 @@ input.addEventListener("keyup", function(e) {
     e.preventDefault();
     if ((e.key === 'Enter')) {
         todoList.addItem(e.target.value, false);
+        input.textContent = "";
     }
 });
+
+document.addEventListener("dragover", function(e) {
+    e.preventDefault();
+})
